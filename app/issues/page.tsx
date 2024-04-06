c0c3e9cf7a702bbcb140e5d3ea5bc9aab58db5d6
Stijn Rutten <stijn.rutten@infosupport.com>
2024-04-06 15:13:55 +0200

    Add styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import IssueStatusBadge from "./components/IssueStatusBadge";
 import delay from "delay";
 import IssueActions from "./components/IssueActions";
-import Link from "next/link";
+import Link from "../components/Link";
 
 const IssuesPage = async () => {
   await delay(2000);
