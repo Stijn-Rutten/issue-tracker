9b72a2b76efeaaae52142d4fdb5d225f398ce415
Stijn Rutten <stijn.rutten@infosupport.com>
2024-04-06 20:02:31 +0200

    Add assignment logic

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,47 +1,67 @@
 "use client";
 import { Skeleton } from "@/app/components";
-import useUsers from "@/app/hooks/useUsers";
+import useUsers from "@/app/issues/hooks/useUsers";
 import { Issue } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const { data: users, error, isLoading } = useUsers();
+
   const [assignedUserId, setAssignedUserId] = useState(issue.assignedToUserId);
+  const [latestAssignedUserId, setLatestAssignedUserId] = useState(
+    issue.assignedToUserId
+  );
 
   useEffect(() => {
-    if (assignedUserId === null) return;
+    if (assignedUserId === null || assignedUserId === latestAssignedUserId) {
+      setLatestAssignedUserId(assignedUserId);
+      return;
+    }
 
-    axios.patch(`/api/issues/${issue.id}`, {
-      assignedToUserId: assignedUserId !== "unassigned" ? assignedUserId : null,
-    });
-    if (assignedUserId === "unassigned") setAssignedUserId(null);
-  }, [assignedUserId, issue]);
+    axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId:
+          assignedUserId !== "unassigned" ? assignedUserId : null,
+      })
+      .then(() => {
+        setLatestAssignedUserId(assignedUserId);
+        if (assignedUserId === "unassigned") setAssignedUserId(null);
+      })
+      .catch(() => {
+        toast.error("Changes could not be saved");
+        setAssignedUserId(latestAssignedUserId);
+      });
+  }, [assignedUserId, latestAssignedUserId, issue]);
 
   if (isLoading) return <Skeleton height="2rem" />;
 
   if (error) return null;
 
   return (
-    <Select.Root
-      value={assignedUserId ?? ""}
-      defaultValue={issue.assignedToUserId ? issue.assignedToUserId : ""}
-      onValueChange={setAssignedUserId}
-    >
-      <Select.Trigger placeholder="Asign..." />
-      <Select.Content>
-        <Select.Group>
-          <Select.Item value="unassigned">Unassign</Select.Item>
-          <Select.Label>Suggestions</Select.Label>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        value={latestAssignedUserId || ""}
+        defaultValue={issue.assignedToUserId || ""}
+        onValueChange={setAssignedUserId}
+      >
+        <Select.Trigger placeholder="Assign..." />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassign</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster></Toaster>
+    </>
   );
 };
 
