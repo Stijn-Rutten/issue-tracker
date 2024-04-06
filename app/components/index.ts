090413e306c5aa1dcbe20a8fb63c55e462f3d79a
Stijn Rutten <stijn.rutten@infosupport.com>
2024-04-06 15:36:37 +0200

    Clean up skeleton usage

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,9 +1,5 @@
-import Link from "./Link";
-import ErrorMessage from "./ErrorMessage";
-import Spinner from "./Spinner";
-import IssueStatusBadge from "./IssueStatusBadge";
-
-export { Link };
-export { ErrorMessage };
-export { Spinner };
-export { IssueStatusBadge };
+export { default as Link } from "./Link";
+export { default as ErrorMessage } from "./ErrorMessage";
+export { default as Spinner } from "./Spinner";
+export { default as IssueStatusBadge } from "./IssueStatusBadge";
+export { default as Skeleton } from "./Skeleton";
