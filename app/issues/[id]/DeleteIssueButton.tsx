4afb1130f502bdacaf49685965e2819ec76c7766
Stijn Rutten <stijn.rutten@infosupport.com>
2024-04-06 16:54:02 +0200

    Add loading state on deleting issues

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Spinner } from "@/app/components";
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -7,14 +8,18 @@ import { useState } from "react";
 const DeleteIssueButton = ({ id }: { id: number }) => {
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const deleteIssue = async () => {
+    setIsLoading(true);
     try {
       await axios.delete(`/api/issues/${id}`);
       router.push("/issues");
       router.refresh();
     } catch (error) {
       setError(true);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -22,7 +27,9 @@ const DeleteIssueButton = ({ id }: { id: number }) => {
     <>
       <AlertDialog.Root>
         <AlertDialog.Trigger>
-          <Button color="red">Delete Issue</Button>
+          <Button color="red" disabled={isLoading}>
+            Delete Issue {isLoading && <Spinner />}
+          </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
           <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
