205d171c7ea7557048e639c18a9769c700e10969
Stijn Rutten <stijn.rutten@infosupport.com>
2024-04-17 20:02:03 +0200

    Add playwright

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from '../components';
+
+const page = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <Link href='/'>Home</Link>
+    </>
+  );
+};
+
+export default page;
